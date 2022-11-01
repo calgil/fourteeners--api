e2298e39a071000c2233015aed2312a7ccf469a5
calgil <clgilbert1105@gmail.com>
2022-11-01 15:57:11 -0600

    increase results per page

--- middleware/filteredResults.js
+++ middleware/filteredResults.js
@@ -22,7 +22,7 @@ const filteredResults = (model) => async (req, res, next) => {
     }
 
     const page = parseInt(req.query.page, 10) || 1;
-    const limit = parseInt(req.query.limit, 10) || 25;
+    const limit = parseInt(req.query.limit, 10) || 60;
     const startIndex = (page - 1) * limit;
     const endIndex = page * limit;
     const total = await model.countDocuments();
